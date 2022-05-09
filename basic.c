d00df7cd4f89037dfad796151c10c437a3ebf1bb
Kyutae <boxo0987@naver.com>
2022-05-09 12:30:32 +0900

    selectButton

--- basic.c
+++ basic.c
@@ -1,30 +1,18 @@
 #include "basic.h"
 
-void readMenu(Menu *m){
-    printf("%-8s %5d원 %4s\n",m->food,p->price,p->booth);
-}
-
-int loadData(Menu *m)
+int selectButton()
 {
- int count = 0;
- FILE *fp;
-
- fp = fopen("Product.txt","rt");
- if(fp==NULL) printf("=>파일 없음\n");
- else
-{
- for(;count<100;count++)
- {
- fscanf(fp,"%[^\n]s.",m[count].food);
- if(feof(fp)) break;
- fscanf(fp,"%d",&m[count].price);
- fscanf(fp,"%[^\n]s",m[count].booth);
- }
- fclose(fp);
- printf("=>로딩 성공!");
- printf("%d",count);
-}
- return count;
+    int button;
+    printf("\n*** 제품 관리 ***\n");
+    printf("1. 조회\n");
+    printf("2. 추가\n");
+    printf("3. 수정\n");
+    printf("4. 삭제\n");
+    printf("5. 저장\n");
+    printf("0. 종료\n\n");
+    printf("=> 원하는 메뉴는? ");
+    scanf("%d", &button);
+    return button;
 }
 
 int createMenu(Menu *m)
@@ -42,13 +30,16 @@ int createMenu(Menu *m)
  return 1;
 }
 
+void readMenu(Menu m){
+    printf("%-8s %5d원 %4s\n",m.food,m.price,m.booth);
+}
 void listMenu(Menu *m, int count)
 {
  for(int i = 0; i < count; i++)
  {
   if(m[i].price == -1) continue;
   printf("========== %d ==========\n", i+1);
-  readMenu(p[i]);
+  readMenu(m[i]);
   printf("========================\n");
  }
 }
@@ -77,4 +68,57 @@ int updateMenu(Menu *m)
 
  return 1;
 }
+
+int loadData(Menu *m)
+{
+ int count = 0;
+ FILE *fp;
+
+ fp = fopen("Product.txt","rt");
+ if(fp==NULL) printf("=>파일 없음\n");
+ else
+{
+ for(;count<100;count++)
+ {
+ fscanf(fp,"%[^\n]s.",m[count].food);
+ if(feof(fp)) break;
+ fscanf(fp,"%d",&m[count].price);
+ fscanf(fp,"%[^\n]s",m[count].booth);
+ }
+ fclose(fp);
+ printf("=>로딩 성공!");
+ printf("%d",count);
+}
+ return count;
+}
+
+int deleteMenu(Menu *m);
+
+int loadData(Menu *m)
+{
+ int count = 0;
+ FILE *fp;
+
+ fp = fopen("Product.txt","rt");
+ if(fp==NULL) printf("=>파일 없음\n");
+ else
+{
+ for(;count<100;count++)
+ {
+ fscanf(fp,"%[^\n]s.",m[count].food);
+ if(feof(fp)) break;
+ fscanf(fp,"%d",&m[count].price);
+ fscanf(fp,"%[^\n]s",m[count].booth);
+ }
+ fclose(fp);
+ printf("=>로딩 성공!");
+ printf("%d",count);
+}
+ return count;
+}
+
+void saveData(Menu *m, int count);
+
+
+
 
